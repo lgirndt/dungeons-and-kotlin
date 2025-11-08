675cefab2d863094f8f33a3bc65b4fc75b7ceca9
Lars Girndt <l.girndt@googlemail.com>
2025-11-08 11:52:31 +0100

    cleanup code

--- src/main/kotlin/CharacterClass.kt
+++ src/main/kotlin/CharacterClass.kt
@@ -9,18 +9,6 @@ open class CharacterClass(
         get() = this::class.simpleName!!
 
     fun isProficientWith(weapon: Weapon): Boolean = weaponProficiency(weapon.category, weapon)
-
-//    data object Cleric : CharacterClass(Die.D8, WeaponProficiencies.simple)
-//    data object Druid : CharacterClass(Die.D8, WeaponProficiencies.simple)
-//    data object Barbarian : CharacterClass(Die.D12, WeaponProficiencies.all)
-//    data object Paladin : CharacterClass(Die.D10)
-//    data object Ranger : CharacterClass(Die.D10)
-//    data object Rogue : CharacterClass(Die.D8)
-//    data object Warlock : CharacterClass(Die.D8)
-//    data object Monk : CharacterClass(Die.D8)
-//    data object Sorcerer : CharacterClass(Die.D6)
-//    data object Bard : CharacterClass(Die.D8, WeaponProficiencies.simple)
-//    data object Wizard : CharacterClass(Die.D8)
 }
 
 // missing classes
