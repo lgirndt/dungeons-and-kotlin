47ed24458695caa289587c9cc0642d74cc592802
Lars Girndt <l.girndt@googlemail.com>
2025-11-09 09:46:59 +0100

    Sketching a spell

--- src/main/kotlin/spell/Spell.kt
+++ src/main/kotlin/spell/Spell.kt
@@ -1,5 +1,7 @@
 package org.example.spell
 
+import org.example.*
+
 enum class SpellSchool {
     Abjuration,
     Conjuration,
@@ -12,48 +14,63 @@ sealed class SpellLevel(val level: Int) : Comparable<SpellLevel> {
     override fun compareTo(other: SpellLevel): Int = compareBy(SpellLevel::level).compare(this, other)
 
     object Cantrip : SpellLevel(0)
-    object Level1: SpellLevel(1)
+    object Level1 : SpellLevel(1)
 }
 
 interface Caster {
-
+    val spellCastingAbility: Stat
+    val proficiencyBonus: ProficiencyBonus
+    val position: Coordinate
+    val stats: StatBlock
 }
 
-interface SpellAffectable {
-
-}
-
-fun cast(caster: Caster, spell: Spell, onLevel: SpellLevel) {
+fun castRangeAttackSpell(
+    caster: Caster,
+    opponent: Attackable,
+    spell: AttackSpell,
+    onLevel: SpellLevel,
+    rollModifier: RollModifier
+) {
     if (onLevel == SpellLevel.Cantrip && spell.level != SpellLevel.Cantrip) {
         return
     }
-    if(spell.level < onLevel) {
+    if (spell.level < onLevel) {
         return
     }
+    val spellAsWeapon = spellAsWeapon(caster, spell)
+    attack(object : Attacker {
+        override val currentWeapon: Weapon? = spellAsWeapon
+        override val position: Coordinate = caster.position
+        override val stats: StatBlock = caster.stats
+        override fun applyAttackModifiers(weapon: Weapon): Int =
+            caster.spellCastingAbility.modifier + caster.proficiencyBonus.toInt()
+
+        override fun isCriticalHit(hitRoll: DieRoll): Boolean = hitRoll.value == 20
+
+    }, opponent, rollModifier)
+
 }
 
-open class Spell(
+private fun spellAsWeapon(caster: Caster, spell: AttackSpell): Weapon {
+    return Weapon(
+        name = spell.name,
+        category = WeaponCategory.Simple, // TODO this does not make sense
+        attackType = AttackType.Ranged,
+        damageType = spell.damageType, // TODO get from spell
+        modifierStat = { caster.spellCastingAbility },
+        damageRoll = spell.damageRoll,
+    )
+
+}
+
+
+class AttackSpell(
     val name: String,
     val school: SpellSchool,
     val level: SpellLevel,
-) {
-}
-open class AttackSpell(
-    name: String,
-    school: SpellSchool,
-    level: SpellLevel,
-) : Spell(
-    name,
-    school,
-    level,
-) {
-
-}
-
-
-class FireBolt : Spell(
-    name = "Fire Bolt",
-    school = SpellSchool.Evocation,
-    level = SpellLevel.Cantrip,
+    val damageType: DamageType,
+    val damageRoll: DamageRoll
 )
+
+
 
