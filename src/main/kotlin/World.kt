4ba085ecca113724e59ba76f934006a63d5dd603
Lars Girndt <l.girndt@googlemail.com>
2025-11-12 13:12:54 +0100

    Introduce Feet

--- src/main/kotlin/World.kt
+++ src/main/kotlin/World.kt
@@ -2,6 +2,25 @@ package org.example
 
 import kotlin.math.sqrt
 
+@JvmInline
+value class Feet(val value: Double) {
+    operator fun plus(other: Feet): Feet {
+        return Feet(this.value + other.value)
+    }
+
+    operator fun minus(other: Feet): Feet {
+        return Feet(this.value - other.value)
+    }
+
+    operator fun times(other: Feet): Feet {
+        return Feet(this.value * other.value)
+    }
+
+    operator fun div(other: Feet): Feet {
+        return Feet(this.value / other.value)
+    }
+}
+
 data class Coordinate(val x: Int, val y: Int) {
     operator fun plus(other: Coordinate): Coordinate {
         return Coordinate(this.x + other.x, this.y + other.y)
