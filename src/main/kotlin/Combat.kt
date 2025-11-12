8fc4859534c21a6bbd4ef4bdf0383e6789e5abf3
Lars Girndt <l.girndt@googlemail.com>
2025-11-12 16:55:40 +0100

    WIP CombatScenario

--- src/main/kotlin/Combat.kt
+++ src/main/kotlin/Combat.kt
@@ -8,12 +8,12 @@ enum class FactionStance {
     Hostile
 }
 
-data class Faction (
+data class Faction(
     val id: Id<Faction> = Id.generate(),
     val name: String,
 )
 
-data class FactionRelationship (
+data class FactionRelationship(
     val first: Faction,
     val second: Faction,
     val stance: FactionStance
@@ -48,7 +48,7 @@ class FactionRelations private constructor(
             if (key in relationships) {
                 throw IllegalArgumentException("Relationship $relationshipToAdd already exists.")
             }
-            if ( relationshipToAdd.stance != FactionRelations.DEFAULT_STANCE) {
+            if (relationshipToAdd.stance != FactionRelations.DEFAULT_STANCE) {
                 relationships[key] = relationshipToAdd.stance
             }
             return this
@@ -60,35 +60,34 @@ class FactionRelations private constructor(
     }
 }
 
-data class Combatant (
+data class Combatant(
     val entity: CoreEntity,
     val faction: Faction
 )
 
-class CombatantsStore (
+class CombatantsStore(
     combatantsByFaction: Multimap<Faction, CoreEntity>,
     nonHostileFactionRelationships: List<FactionRelationship> = emptyList(),
 ) {
-    val combatants : Map<Id<CoreEntity>, Combatant> = combatantsByFaction.entries()
+    val combatants: Map<Id<CoreEntity>, Combatant> = combatantsByFaction.entries()
         .map { Combatant(faction = it.key, entity = it.value) }
         .associateBy { it.entity.id }
 
     val factionRelations: FactionRelations = nonHostileFactionRelationships
         .onEach {
-            require (it.stance == FactionStance.Hostile) {
+            require(it.stance == FactionStance.Hostile) {
                 "CombatantsStore can only accept non-hostile relationships."
             }
         }
-        .fold(FactionRelations.Builder()) {
-        builder, relationship ->
-        builder.add(relationship)
-    }.build()
+        .fold(FactionRelations.Builder()) { builder, relationship ->
+            builder.add(relationship)
+        }.build()
 
-    fun findOrNull(id: Id<CoreEntity>) : Combatant? {
+    fun findOrNull(id: Id<CoreEntity>): Combatant? {
         return combatants[id]
     }
 
-    fun findAllWithStance(towards: Id<CoreEntity>, stance: FactionStance) : List<Combatant> {
+    fun findAllWithStance(towards: Id<CoreEntity>, stance: FactionStance): List<Combatant> {
         val combatant = combatants[towards] ?: return emptyList()
         val targetFaction = combatant.faction
         return combatants.values.filter {
@@ -96,4 +95,42 @@ class CombatantsStore (
             relationStance == stance
         }
     }
+
+    fun listAll(): List<Combatant> {
+        return combatants.values.toList()
+    }
+}
+
+interface CombatScenario {
+    fun listVisibleCombatants(observer: Id<CoreEntity>): List<Combatant>
+    fun isVisibleTo(observer: Id<CoreEntity>, target: Id<CoreEntity>): Boolean
+    fun listCombatantsInRange(observer: Id<CoreEntity>, rangeInFeet: Feet): List<Combatant>
+}
+
+class SimpleCombatScenario(
+    private val combatantsStore: CombatantsStore
+) : CombatScenario {
+
+    override fun listVisibleCombatants(observer: Id<CoreEntity>): List<Combatant> {
+        return combatantsStore.listAll().filter { it.entity.id != observer }
+    }
+
+    override fun isVisibleTo(
+        observer: Id<CoreEntity>,
+        target: Id<CoreEntity>
+    ): Boolean = true
+
+
+    override fun listCombatantsInRange(
+        observer: Id<CoreEntity>,
+        rangeInFeet: Feet
+    ): List<Combatant> {
+        val entity = combatantsStore.findOrNull(observer)?.entity
+            ?: return emptyList()
+
+        return combatantsStore.listAll()
+            .filter { it.entity.id != observer }
+            .filter { isInRange(entity.position, it.entity.position, rangeInFeet) }
+    }
+
 }
