c7c7a0e3ffbeda8ab033dade13e97aeaa2d5cc54
Lars Girndt <l.girndt@googlemail.com>
2025-11-14 20:51:00 +0100

    app needs to know domain

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,13 +5,14 @@ plugins {
 group = "io.dungeons"
 version = "1.0-SNAPSHOT"
 
-//dependencies {
-//    implementation("com.google.guava:guava:${Versions.guava}")
-//
+dependencies {
+    implementation("com.google.guava:guava:${Versions.guava}")
+    implementation((project(":dungeons-domain")))
+
 //    testImplementation(kotlin("test"))
 //    testImplementation(platform("org.junit:junit-bom:${Versions.junitBom}"))
 //    testImplementation("org.junit.jupiter:junit-jupiter")
 //    testImplementation("com.natpryce:hamkrest:${Versions.hamkrest}")
 //    testImplementation("io.mockk:mockk:${Versions.mockk}")
 //    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-//}
+}
