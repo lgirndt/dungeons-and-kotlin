8f2100431ad64d42f6c4e8f445f8ae42369aa6cb
Lars Girndt <l.girndt@googlemail.com>
2025-11-14 20:51:19 +0100

    A Main Class

---
+++ app/src/main/kotlin/Main.kt
@@ -0,0 +1,40 @@
+
+import io.dungeons.*
+
+fun main() {
+    val fighter = PlayerCharacter(
+        data = PlayerCharacterData(
+            level = 5,
+            weapon = Weapons.LONGSWORD
+        ),
+        classFeatures = Fighter(),
+        core = CoreEntityData(
+            name = "Aragorn",
+            stats = StatBlock.create(
+                str = 16,
+                dex = 13,
+                con = 14,
+                int = 10,
+                wis = 12,
+                cha = 14
+            ),
+            hitPoints = 15,
+            armourClass = 13,
+            damageModifiers = DamageModifiers.NONE,
+        )
+    )
+
+    println("Hello, ${fighter.name}! You've reached Nowhere.")
+
+    val goblin = Goblin()
+    println("A wild ${goblin.name} appears!")
+
+    println("${fighter.name} attacks the ${goblin.name} with a ${fighter.weapon.name}.")
+    val outcome = fighter.attack(goblin)
+    println("${fighter.name} rolled a ${outcome.hitRoll}")
+    if (outcome.hasBeenHit) {
+        println("${fighter.name} hits the ${goblin.name} for ${outcome.damageDealt} damage!")
+    } else {
+        println("${fighter.name} misses the ${goblin.name}.")
+    }
+}
