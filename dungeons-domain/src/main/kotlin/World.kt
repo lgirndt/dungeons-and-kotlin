59adaf8c5dcd6baf568946d61a0c92a1f28f3e4e
Lars Girndt <l.girndt@googlemail.com>
2025-11-17 17:03:12 +0100

    Add Square

--- dungeons-domain/src/main/kotlin/World.kt
+++ dungeons-domain/src/main/kotlin/World.kt
@@ -32,6 +32,40 @@ value class Feet(val value: Double) : Comparable<Feet> {
     }
 }
 
+@JvmInline
+value class Square(val value: Int) : Comparable<Square> {
+
+    operator fun plus(other: Square): Square {
+        return Square(this.value + other.value)
+    }
+
+    operator fun minus(other: Square): Square {
+        return Square(this.value - other.value)
+    }
+
+    operator fun times(factor: Int): Square {
+        return Square(this.value * factor)
+    }
+
+    operator fun div(divisor: Int): Square {
+        return Square(this.value / divisor)
+    }
+
+    fun toFeet(): Feet {
+        return Feet(this.value * FEET_PER_SQUARE)
+    }
+
+    override fun compareTo(other: Square): Int {
+        return this.value.compareTo(other.value)
+    }
+
+    companion object {
+        const val FEET_PER_SQUARE = 5.0
+    }
+}
+
+
+
 data class Coordinate(val x: Feet, val y: Feet) {
     operator fun plus(other: Coordinate): Coordinate {
         return Coordinate(this.x + other.x, this.y + other.y)
