c39f15bb8b3846c29700c60cd87d3b958c8048b5
Lars Girndt <l.girndt@googlemail.com>
2025-11-17 11:01:53 +0100

    Add Kobold Warrior

--- dungeons-domain/src/main/kotlin/Monster.kt
+++ dungeons-domain/src/main/kotlin/Monster.kt
@@ -30,3 +30,23 @@ class Goblin(
     override val attackModifier: Int = weapon.whichStat(core.stats).modifier,
 
 ) : Monster(id, core, weapon, attackModifier)
+
+class KoboldWarrior(
+    id: Id<CoreEntity> = Id.generate(),
+    core: CoreEntityData = CoreEntityData(
+        name = "Kobold Warrior",
+        stats = StatBlock(
+            str = Stat(7),
+            dex = Stat(15),
+            con = Stat(9),
+            int = Stat(8),
+            wis = Stat(7),
+            cha = Stat(8),
+        ),
+        hitPoints = 7,
+        armourClass = 14,
+        damageModifiers = DamageModifiers.NONE,
+    ),
+    override val weapon: Weapon = Weapons.Dagger,
+    override val attackModifier: Int = weapon.whichStat(core.stats).modifier,
+) : Monster(id, core, weapon, attackModifier)
