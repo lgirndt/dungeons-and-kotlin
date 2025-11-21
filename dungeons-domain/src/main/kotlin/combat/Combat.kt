a4cb03e07a6f0bb3aa0d3490d976076f6b24e0eb
Lars Girndt <l.girndt@googlemail.com>
2025-11-21 20:02:53 +0100

    Extract CombatScenario

--- dungeons-domain/src/main/kotlin/combat/Combat.kt
+++ dungeons-domain/src/main/kotlin/combat/Combat.kt
@@ -4,8 +4,6 @@ import io.dungeons.Creature
 import io.dungeons.DieRoll
 import io.dungeons.board.BoardPosition
 import io.dungeons.core.Id
-import io.dungeons.world.Square
-import io.dungeons.world.isInRange
 
 enum class FactionStance {
     Friendly,
@@ -132,47 +130,4 @@ data class Combatant(
 
 interface ProvidesBoardPosition {
     fun getBoardPosition(creatureId: Id<Creature>): BoardPosition?
-}
-
-interface CombatScenario : ProvidesBoardPosition {
-    fun listVisibleCombatants(observer: Id<Creature>): List<Combatant>
-    fun isVisibleTo(observer: Id<Creature>, target: Id<Creature>): Boolean
-    fun listCombatantsInRange(observer: Id<Creature>, rangeInSquares: Square): List<Combatant>
-    override fun getBoardPosition(creatureId: Id<Creature>): BoardPosition?
-}
-
-class SimpleCombatScenario(
-    private val combatantsCollection: CombatantsCollection
-) : CombatScenario {
-
-    override fun listVisibleCombatants(observer: Id<Creature>): List<Combatant> {
-        return combatantsCollection.listAll().filter { it.id != observer }
-    }
-
-    override fun isVisibleTo(
-        observer: Id<Creature>,
-        target: Id<Creature>
-    ): Boolean = true
-
-
-    override fun listCombatantsInRange(
-        observer: Id<Creature>,
-        rangeInSquares: Square
-    ): List<Combatant> {
-        val position = getBoardPosition(observer)
-            ?: error("No grid position found for creature $observer")
-
-        return combatantsCollection.listAll()
-            .filter { it.id != observer }
-            .filter {
-                getBoardPosition(it.creature.id)?.let { targetPos ->
-                    isInRange(position, targetPos, rangeInSquares)
-                } ?: true
-            }
-    }
-
-    override fun getBoardPosition(creatureId: Id<Creature>): BoardPosition? {
-        TODO("Not yet implemented")
-    }
-
 }
