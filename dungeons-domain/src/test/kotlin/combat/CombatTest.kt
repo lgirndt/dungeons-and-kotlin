9ac93b334236363a29b6e5106e61e40fa3a35919
Lars Girndt <l.girndt@googlemail.com>
2025-11-25 19:37:01 +0100

    Yet another fix

--- dungeons-domain/src/test/kotlin/combat/CombatTest.kt
+++ dungeons-domain/src/test/kotlin/combat/CombatTest.kt
@@ -1,13 +1,13 @@
 package combat
 import TestId
 import aPlayerCharacter
-import org.junit.jupiter.api.Assertions.assertEquals
 import fromModifiers
 import io.dungeons.Creature
 import io.dungeons.Die.Companion.D20
 import io.dungeons.PlayerCharacter
 import io.dungeons.StatBlock
 import io.dungeons.combat.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertNotNull
