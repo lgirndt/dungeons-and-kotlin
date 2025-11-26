7331a9584349f9c2ea6c534dc15a7529416ef077
Lars Girndt <l.girndt@googlemail.com>
2025-11-26 09:09:49 +0100

    Remove commented stuff

--- buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-conventions.gradle.kts
@@ -41,11 +41,3 @@ tasks.withType<Test>().configureEach {
         }
     }
 }
-
-//dependencies {
-//    testImplementation(platform("org.junit:junit-bom:${Versions.junitBom}"))
-//    testImplementation("org.junit.jupiter:junit-jupiter")
-//    testImplementation("com.natpryce:hamkrest:${Versions.hamkrest}")
-//    testImplementation("io.mockk:mockk:${Versions.mockk}")
-//    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-//}
