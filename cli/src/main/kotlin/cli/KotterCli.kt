49e25f3d4e0b5852534fe1caa64a90bf161077a8
Lars Girndt <l.girndt@googlemail.com>
2025-12-06 11:37:41 +0100

    Make it Springy

--- cli/src/main/kotlin/cli/KotterCli.kt
+++ cli/src/main/kotlin/cli/KotterCli.kt
@@ -2,6 +2,7 @@ package cli
 
 import cli.screen.DetailsScreen
 import cli.screen.MyScreen
+import cli.screen.Screen
 import cli.screen.ScreenTransition
 import com.varabyte.kotter.foundation.firstSuccess
 import com.varabyte.kotter.foundation.session
@@ -16,25 +17,41 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 
+typealias ScreenMap = Map<ScreenTransition, Screen<ScreenTransition>>
+
 @SpringBootApplication
 class KotterCli {
 
     private val logger = LoggerFactory.getLogger(KotterCli::class.java)
 
     @Bean
-    fun runner() = CommandLineRunner {
-        println("CLI Application started! What do you want to do?")
+    fun myScreen() = MyScreen()
+
+    @Bean
+    fun detailsScreen() = DetailsScreen()
+
+    @Bean
+    fun screens(
+        myScreen: MyScreen,
+        detailsScreen: DetailsScreen,
+    )
+        : ScreenMap {
+        // It makes much more sense to build this lazy
+        return listOf(
+            myScreen,
+            detailsScreen,
+        ).associateBy { it.ownTransition }
+    }
+
+
+    @Bean
+    fun runner(screens: ScreenMap) = CommandLineRunner {
         session(
             terminal = listOf(
                 { SystemTerminal() },
-                { VirtualTerminal.create(title = "My App", terminalSize = TerminalSize(80, 40)) },
+                { VirtualTerminal.create(title = "D&D", terminalSize = TerminalSize(80, 40)) },
             ).firstSuccess(),
         ) {
-            val screens = listOf(
-                MyScreen(),
-                DetailsScreen(),
-            ).associateBy { it.ownTransition }
-
             var transition = ScreenTransition.MyScreen
             while (transition != ScreenTransition.Exit) {
                 clearScreen()
@@ -55,28 +72,7 @@ private fun Session.clearScreen() {
 }
 
 fun main(args: Array<String>) {
+    // Otherwise the VirtualTerminal may fail to initialize on some systems
     System.setProperty("java.awt.headless", "false")
     runApplication<KotterCli>(*args)
-//    session(
-//        terminal = listOf(
-//            { SystemTerminal() },
-//            { VirtualTerminal.create(title = "My App", terminalSize = TerminalSize(80, 40)) },
-//        ).firstSuccess(),
-//    ) {
-//
-//        val screens = listOf(
-//            MyScreen(this),
-//            DetailsScreen(this),
-//        ).associateBy { it.ownTransition }
-//
-//        var transition = ScreenTransition.MyScreen
-//        while (transition != ScreenTransition.Exit) {
-//            clearScreen()
-//            val screen = screens[transition] ?:
-//                break
-//
-//            transition = screen.run()
-//        }
-//    }
-
 }
