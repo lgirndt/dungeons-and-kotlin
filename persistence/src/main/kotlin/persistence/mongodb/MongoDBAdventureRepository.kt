eef1ffda12d64e6f7c126f0d47594b0ca7ee15d4
Lars Girndt <l.girndt@googlemail.com>
2025-12-09 11:43:37 +0100

    WIP ok, we use Domain objects as entities as well. Let's see where we and up here

--- persistence/src/main/kotlin/persistence/mongodb/MongoDBAdventureRepository.kt
+++ persistence/src/main/kotlin/persistence/mongodb/MongoDBAdventureRepository.kt
@@ -3,18 +3,10 @@ package io.dungeons.persistence.mongodb
 import io.dungeons.domain.adventure.Adventure
 import io.dungeons.domain.adventure.AdventureRepository
 import io.dungeons.domain.core.Id
-import io.dungeons.persistence.mongodb.entities.AdventureDocument
-import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
-import java.util.*
 
 @Repository
-class MongoDBAdventureRepository(private val mongoTemplate: MongoTemplate) : AdventureRepository {
-    override fun findAll(): List<Adventure> = mongoTemplate
-        .findAll(AdventureDocument::class.java)
-        .map { it.toDomain() }
-
-    override fun findById(id: Id<Adventure>): Optional<Adventure> {
-        TODO("Not yet implemented")
-    }
-}
+interface MongoDBAdventureRepository :
+    AdventureRepository,
+    MongoRepository<Adventure, Id<Adventure>>
