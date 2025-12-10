30e47c695baf4d8af2dd10eee0dcde93d8a15d77
Lars Girndt <l.girndt@googlemail.com>
2025-12-10 10:06:36 +0100

    Fix claude ideas, that are not necessary

--- dungeons-domain/src/main/kotlin/domain/savegame/SaveGame.kt
+++ dungeons-domain/src/main/kotlin/domain/savegame/SaveGame.kt
@@ -4,10 +4,9 @@ import io.dungeons.domain.adventure.Adventure
 import io.dungeons.domain.core.Id
 import io.dungeons.domain.core.Player
 import io.dungeons.domain.world.Room
-import org.springframework.data.annotation.Id as MongoId
 
 data class SaveGame(
-    @MongoId val id: Id<SaveGame> = Id.generate(),
+    val id: Id<SaveGame> = Id.generate(),
     val userId: Id<Player>,
     val adventureId: Id<Adventure>,
     val currentRoomId: Id<Room>,
--- persistence/src/test/kotlin/persistence/mongodb/TestConfiguration.kt
+++ persistence/src/test/kotlin/persistence/mongodb/TestConfiguration.kt
@@ -1,6 +1,13 @@
 package io.dungeons.persistence.mongodb
 
-import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.SpringBootConfiguration
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
-@SpringBootApplication
-class TestApplication
+// DataMongoTest are looking for this annotation
+@SpringBootConfiguration()
+// This only scans for Mongo Repositories, now other components
+// We cannot instantiate repositories on our own, since the only exist as interfaces.
+@EnableMongoRepositories(basePackages = ["io.dungeons.persistence.mongodb"])
+class TestConfiguration {
+
+}
