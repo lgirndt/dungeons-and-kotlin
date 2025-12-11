ca9154d6927c25284c47f27794ba261722744a3f
Lars Girndt <l.girndt@googlemail.com>
2025-12-11 18:05:10 +0100

    Properly set up dev user in mongo

---
+++ docker/mongo-init.js
@@ -0,0 +1,15 @@
+// MongoDB initialization script to create dev user for dungeon database
+db = db.getSiblingDB('dungeon');
+
+db.createUser({
+  user: 'dev',
+  pwd: 'dev',
+  roles: [
+    {
+      role: 'readWrite',
+      db: 'dungeon'
+    }
+  ]
+});
+
+print('✅ Created dev user with readWrite access to dungeon database');
