773d89928fe8b889a6ed24fca09bb557c93c631a
Lars Girndt <l.girndt@googlemail.com>
2025-12-15 16:47:47 +0100

    Even more Kotlin idiomic

--- api/src/main/kotlin/api/security/PlayerDetailsService.kt
+++ api/src/main/kotlin/api/security/PlayerDetailsService.kt
@@ -3,23 +3,24 @@ package io.dungeons.api.security
 import io.dungeons.domain.player.Player
 import io.dungeons.domain.player.PlayerRepository
 import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import kotlin.jvm.optionals.getOrElse
 
+private fun Player.toPlayerDetails() = PlayerDetails(
+    playerId = this.id.toUUID(),
+    username = this.name,
+    password = this.hashedPassword,
+    authorities = listOf(
+        SimpleGrantedAuthority("ROLE_USER"),
+    ),
+)
+
 class PlayerDetailsService(private val playerRepository: PlayerRepository) : UserDetailsService {
-    override fun loadUserByUsername(username: String): UserDetails {
-        val player: Player = playerRepository.findByName(username).getOrElse {
+    override fun loadUserByUsername(username: String) = playerRepository
+        .findByName(username)
+        .getOrElse {
             throw UsernameNotFoundException(username)
         }
-        return PlayerDetails(
-            playerId = player.id.toUUID(),
-            username = player.name,
-            password = player.hashedPassword,
-            authorities = listOf(
-                SimpleGrantedAuthority("ROLE_USER"),
-            ),
-        )
-    }
+        .toPlayerDetails()
 }
