8513797a5195db38466e7d99b18fd5535c36566a
Lars Girndt <l.girndt@googlemail.com>
2025-12-15 19:29:33 +0100

    Remove the toml file

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,11 +9,12 @@ repositories {
 
 val kotlinVersion = "2.2.20"
 val springBootVersion = "4.0.0"
+val detektVersion = "2.0.0-alpha.1"
 
 dependencies {
     implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:$kotlinVersion")
     implementation("org.jetbrains.kotlin.plugin.spring:org.jetbrains.kotlin.plugin.spring.gradle.plugin:$kotlinVersion")
-    implementation("dev.detekt:detekt-gradle-plugin:${libs.versions.detekt.get()}")
+    implementation("dev.detekt:detekt-gradle-plugin:$detektVersion")
     implementation("org.springframework.boot:org.springframework.boot.gradle.plugin:$springBootVersion")
     implementation("io.spring.gradle:dependency-management-plugin:1.1.7")
 }
