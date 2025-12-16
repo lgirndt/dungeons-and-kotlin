f0f64b4c1737d0853b9b53388e609a815cd24f0a
Lars Girndt <l.girndt@googlemail.com>
2025-12-16 13:51:13 +0100

    Getting GameState from file

--- cli/src/main/kotlin/cli/MainCommand.kt
+++ cli/src/main/kotlin/cli/MainCommand.kt
@@ -1,11 +1,16 @@
 package io.dungeons.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.path
 import org.springframework.stereotype.Component
 
 @Component
 class MainCommand(private val gameLoop: GameLoop) : CliktCommand() {
+    private val gameStateFile by option("--game-state-file")
+        .path(mustExist = true, canBeDir = false)
+
     override fun run() {
-        gameLoop.run(null)
+        gameLoop.run(gameStateFile)
     }
 }
